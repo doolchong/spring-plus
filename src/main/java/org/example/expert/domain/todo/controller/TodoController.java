a66b421a7eaa18c67d5c401699a43bddaa73fa79
Your Name <doolchong@gmail.com>
2024-10-02 18:43:02 +0900

    Level 1-5 - 코드 개선 퀴즈 -  JPA의 이해

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -12,6 +12,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+
 @RestController
 @RequiredArgsConstructor
 public class TodoController {
@@ -29,9 +31,12 @@ public class TodoController {
     @GetMapping("/todos")
     public ResponseEntity<Page<TodoResponse>> getTodos(
             @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "10") int size
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "") String weather,
+            @RequestParam(defaultValue = "1900-01-01") LocalDate startedAt,
+            @RequestParam(defaultValue = "2100-12-31") LocalDate endedAt
     ) {
-        return ResponseEntity.ok(todoService.getTodos(page, size));
+        return ResponseEntity.ok(todoService.getTodos(page, size, weather, startedAt, endedAt));
     }
 
     @GetMapping("/todos/{todoId}")
